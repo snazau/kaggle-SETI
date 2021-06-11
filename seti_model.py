21929af2f6f2ef8346504b7e27acc5a2ed6238bf
snazau <snazarikov@gmail.com>
2021-06-11 12:52:17 +0200

    Update seti_model.py
    Add function to compare weights of 2 models

--- seti_model.py
+++ seti_model.py
@@ -123,6 +123,23 @@ def overfit(model):
     print()
 
 
+def compare_models(model_1, model_2):
+    models_differ = 0
+    for key_item_1, key_item_2 in zip(model_1.state_dict().items(), model_2.state_dict().items()):
+        if key_item_1[1].device == key_item_2[1].device and torch.equal(key_item_1[1], key_item_2[1]):
+            pass
+        else:
+            models_differ += 1
+            if (key_item_1[0] == key_item_2[0]):
+                _device = f'device {key_item_1[1].device}, {key_item_2[1].device}' if key_item_1[1].device != \
+                                                                                      key_item_2[1].device else ''
+                print(f'Mismtach {_device} found at', key_item_1[0])
+            else:
+                raise Exception
+    if models_differ == 0:
+        print('Models match perfectly! :)')
+
+
 def get_model_parameter_amount(model):
     parameters_amount = 0
     for parameter in model.parameters():
